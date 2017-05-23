552c7f58f9ef057aabeffca3ae23e76e7a4e3e90
elmotard <elmotard@users.noreply.github.com>
2017-05-23 16:57:46 +0200

    Modification du fichier

--- Ellipse.cpp
+++ Ellipse.cpp
@@ -5,29 +5,30 @@
 Ellipse::Ellipse(ulong color, uint x, uint y, uint _height, uint _width)
  : Forme(color, x, y), height(_height), width(_width)
 {
-  cerr << "Construction d'une Ellipse" << endl;
+    cerr << "Construction d'une Ellipse" << endl;
+    cerr << perimetre();
 }
 
 Ellipse::Ellipse(istream &is)
  : Forme(is), height(0), width(0)
 {
-	is >> height >> width;
+    is >> height >> width;
 }
 
 Ellipse::~Ellipse()
 {
-  cerr << "Destruction de Ellipse";
+    cerr << "Destruction de Ellipse";
 }
 
 double Ellipse::perimetre() const
 {
-	return (2*3.14159265359*(sqrt((1/2)*((getHeight()*getHeight())+(getWidth()*getWidth())))));
+    return (2*3.14159265359*(sqrt((1/2)*((getHeight()*getHeight())+(getWidth()*getWidth())))));
 }
 
 void Ellipse::dessiner(EZWindow &w, bool isActive) const
 {
-	Forme::dessiner(w);
-	w.drawCircle(Forme::getAnchor().getx()-width, Forme::getAnchor().gety()-height, (Forme::getAnchor().getx()+width), (Forme::getAnchor().gety()+height));
+    Forme::dessiner(w);
+    w.drawCircle(Forme::getAnchor().getx()-width, Forme::getAnchor().gety()-height, (Forme::getAnchor().getx()+width), (Forme::getAnchor().gety()+height));
 }
 
 void Ellipse::ecrire(ostream &os) const
